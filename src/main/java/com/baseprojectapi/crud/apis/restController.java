d9078e5105b503e151a56afab097f00d64af6f23
tonyhernandezdelcid <tonyhernandezdelcid@gmail.com>
2023-05-30 10:35:10 -0600

    Se finalizó de agregar la funcionalidad de JPA en el CRUD del microservicio

--- src/main/java/com/baseprojectapi/crud/apis/restController.java
+++ src/main/java/com/baseprojectapi/crud/apis/restController.java
@@ -23,6 +23,7 @@ import com.baseprojectapi.crud.beans.Usuario;
 import com.baseprojectapi.crud.jpa.repository.UsuarioRepository;
 import com.baseprojectapi.dao.DBConnection;
 import com.baseprojectapi.dao.UsuariosDAO;
+import java.util.Optional;
 
 
 @CrossOrigin(origins = "*")
@@ -65,52 +66,98 @@ public class restController {
 	@PostMapping("/crearusuario")
 	public boolean insertaUsuario(@RequestBody String datos) {
 		System.out.println("creando usuario");
-		UsuariosDAO userdao = new UsuariosDAO();
-		Usuario usuario = new Usuario();
+		//UsuariosDAO userdao = new UsuariosDAO();
+		//Usuario usuario = new Usuario();
+		com.baseprojectapi.crud.jpa.model.Usuario usuario = new com.baseprojectapi.crud.jpa.model.Usuario();
 		try {
 			JSONObject object = new JSONObject(datos);
 			usuario.setCodigo(object.getString("codigo"));
 			usuario.setNombre(object.getString("nombre"));
 			usuario.setTelefono(object.getString("telefono"));
+			
+			
+
+			System.out.println("codigo: "+ usuario.getCodigo());
+			System.out.println("nombre: "+ usuario.getNombre());
+			System.out.println("telefono: "+ usuario.getTelefono());
+			
+			
+			usuarioRepository.save(usuario);
+			return true;
 		} catch(Exception ex) {
 			ex.printStackTrace();
+			return false;
 		}
 		
-		System.out.println("codigo: "+ usuario.getCodigo());
-		System.out.println("nombre: "+ usuario.getNombre());
-		System.out.println("telefono: "+ usuario.getTelefono());
 		
-		return userdao.insertarUsuario(usuario);
+		
+		
+		
+		
+		
+		
+		
+		
+		//return userdao.insertarUsuario(usuario);
 	}
 	
 	
 	@PutMapping("/modificarusuario")
 	public boolean modificaUsuario(@RequestBody String datos) {
 		System.out.println("entrando a editar usuarios");
-		UsuariosDAO userdao = new UsuariosDAO();
-		Usuario usuario = new Usuario();
+		
+		com.baseprojectapi.crud.jpa.model.Usuario usuario = new com.baseprojectapi.crud.jpa.model.Usuario();
+		//UsuariosDAO userdao = new UsuariosDAO();
+		//Usuario usuario = new Usuario();
 		try {
 			JSONObject object = new JSONObject(datos);
 			usuario.setCodigo(object.getString("codigo"));
 			usuario.setNombre(object.getString("nombre"));
 			usuario.setTelefono(object.getString("telefono"));
+			
+			System.out.println("codigo mod: "+ usuario.getCodigo());
+			System.out.println("nombre mod: "+ usuario.getNombre());
+			System.out.println("telefono mod: "+ usuario.getTelefono());
+			
+			Optional<com.baseprojectapi.crud.jpa.model.Usuario> usuarioData = usuarioRepository.findById(usuario.getCodigo());
+
+			
+			if (usuarioData.isPresent()) {
+				usuarioRepository.save(usuario);
+				return true;
+			}else {
+				
+				return false;
+			}
+			
+			
+			
+			
 		} catch(Exception ex) {
 			ex.printStackTrace();
+			return false;
 		}
 		
-		System.out.println("codigo mod: "+ usuario.getCodigo());
-		System.out.println("nombre mod: "+ usuario.getNombre());
-		System.out.println("telefono mod: "+ usuario.getTelefono());
-		return userdao.modificarUsuario(usuario);
+		
+		//return userdao.modificarUsuario(usuario);
 	}
 	
 	@DeleteMapping("/eliminausuario/{datos}")
 	public boolean eliminarUsuario(@PathVariable  String datos) {
 		System.out.println("Eliminando usuario "+datos);
-		UsuariosDAO userdao = new UsuariosDAO();
+		//UsuariosDAO userdao = new UsuariosDAO();
 		
 		//boolean inserto = db.insertaBancos(ban);
-		return userdao.eliminarUsuario(datos);
+		//return userdao.eliminarUsuario(datos);
+		
+		try {
+			usuarioRepository.deleteById(datos);
+			return true;
+		}catch(Exception e) {
+			e.printStackTrace();
+			return false;
+		}
+		
 	}
 	
 }
